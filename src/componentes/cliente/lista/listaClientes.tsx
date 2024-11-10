83f8eb4747d7445e1ab3709a549aefbbcc587aaa
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-09 21:00:59 -0300

    Converti a listaCliente para função

--- src/componentes/cliente/lista/listaClientes.tsx
+++ src/componentes/cliente/lista/listaClientes.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./listaClientes.css"
 import Cliente from "../../../modelo/cliente";
 import AlterarCliente from "../alterar/alterarCliente";
@@ -8,121 +7,95 @@ type props = {
     clientes: Cliente[]
 }
 
-type state = {
-    clientes: Cliente[]
-    cliente: Cliente | undefined
-    nome: string
-    nomeSocial: string
-    email: string
-    telefone: string
-    ordemLista: number
-}
+export default function ListaCliente(props: props) {
+    const [clientes, setClientes] = useState<Cliente[]>(props.clientes)
+    const [cliente, setCliente] = useState<Cliente | undefined>(undefined)
+    const [ordemLista, setOrdemLista] = useState<number>(0)
 
-export default class ListaCliente extends Component<props, state> {
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state = {
-            clientes: props.clientes,
-            cliente: undefined,
-            nome: "",
-            nomeSocial: "",
-            email: "",
-            telefone: "",
-            ordemLista: 0
-        }
-        this.gerarListaCliente = this.gerarListaCliente.bind(this)
-        this.excluirCliente = this.excluirCliente.bind(this)
-        this.pegarUmCliente = this.pegarUmCliente.bind(this)
-    }
+    const pegarUmCliente = useCallback((nomeEscolhido: string) => {
+        const cliente = clientes.find(c => c.nome === nomeEscolhido)
+        setCliente(cliente)
+    }, [clientes])
 
-    componentDidMount(): void {
-        this.gerarListaCliente()
-    }
+    const excluirCliente = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>, nome: string) => {
+        const clientesFiltrar = [...clientes]
+        clientesFiltrar.splice((clientes.findIndex(c => c.nome === nome)), 1)
+        setClientes(clientesFiltrar)
+        e.stopPropagation()
+    }, [clientes])
 
-    componentDidUpdate(): void {
-        this.gerarListaCliente()
-    }
-
-    pegarUmCliente(nome: string) {
-        const cliente = this.state.clientes.find(c => c.nome === nome)
-        this.setState({
-            cliente: cliente,
-        })
-    }
-
-    excluirCliente(nome: string) {
-        this.state.clientes.splice((this.state.clientes.findIndex(c => c.nome === nome)), 1)
-    }
-
-    gerarListaCliente() {
-        if (this.state.clientes.length <= 0) {
+    const gerarListaCliente = useCallback(() => {
+        if (clientes.length <= 0) {
             return <></>
         } else {
-            let clientes = this.state.clientes
+            let clientesTemp = clientes
 
-            if (this.state.ordemLista === 0) {
-                clientes = this.props.clientes
-            } else if (this.state.ordemLista === 1) {
-                clientes = clientes.toSorted((a, b) => b.getProdutosConsumidos.length - a.getProdutosConsumidos.length)
-            } else if (this.state.ordemLista === 2) {
-                clientes = clientes.toSorted((a, b) => b.getServicosConsumidos.length - a.getServicosConsumidos.length)
-            } else if (this.state.ordemLista === 3) {
-                clientes = clientes.toSorted((a, b) => b.getValorGasto - a.getValorGasto)
+            if (ordemLista === 0) {
+                clientesTemp = clientes
+            } else if (ordemLista === 1) {
+                clientesTemp = clientes.toSorted((a, b) => b.getProdutosConsumidos.length - a.getProdutosConsumidos.length)
+            } else if (ordemLista === 2) {
+                clientesTemp = clientes.toSorted((a, b) => b.getServicosConsumidos.length - a.getServicosConsumidos.length)
+            } else if (ordemLista === 3) {
+                clientesTemp = clientes.toSorted((a, b) => b.getValorGasto - a.getValorGasto)
             }
 
-            let listaCliente = clientes.map((c, i) =>
-                <tr className="linhaTabelaClientes" key={i} onClick={() => this.pegarUmCliente(c.nome)
+            let listaCliente = clientesTemp.map((c, i) =>
+                <tr className="linhaTabelaClientes" key={i} onClick={() => pegarUmCliente(c.nome)
                 }>
                     <td>{c.nome}</td>
                     <td>{c.nomeSocial}</td>
                     <td>{c.getCpf.getDataEmissao.toISOString().split("T")[0]} {c.getCpf.getValor}</td>
-                    <td><button className="botaExcluirCliente" onClick={() => this.excluirCliente(c.nome)}>Excluir</button></td>
+                    <td><button className="botaExcluirCliente" onClick={(e) => excluirCliente(e, c.nome)}>Excluir</button></td>
                 </tr>
             )
             return listaCliente
         }
-    }
+    }, [ordemLista, clientes, excluirCliente, pegarUmCliente])
 
-    render() {
-        return (
-            <div className="containerListaCliente">
-                {this.state.cliente === undefined ? (
-                    <div className="clientesCadastrados">
-                        <select className="seletorOrdemListaCliente"
-                            onChange={e => this.setState({ ordemLista: Number(e.target.value).valueOf() })}
-                        >
-                            <option value={0}>Ordenar por ordem cadastrado</option>
-                            <option value={1}>Ordenar por qtd produtos consumidos</option>
-                            <option value={2}>Ordenar por qtd serviços consumidos</option>
-                            <option value={3}>Ordenar por valor gasto</option>
-                        </select>
+    useEffect(() => {
+        gerarListaCliente()
+    }, [gerarListaCliente])
 
-                        <table className="tabelaClientes">
-                            <thead>
-                                <tr className="headerTabelaClientes">
-                                    <th>Nome</th>
-                                    <th>Nome Social</th>
-                                    <th>CPF</th>
-                                    <th>Excluir</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.gerarListaCliente()}
-                            </tbody>
-                        </table>
-                    </div>
+    return (
+        <div className="containerListaCliente">
+            {cliente === undefined ? (
+                <div className="clientesCadastrados">
+                    <select className="seletorOrdemListaCliente"
+                        onChange={e => setOrdemLista(Number(e.target.value).valueOf())}
+                    >
+                        <option value={0}>Ordenar por ordem cadastrado</option>
+                        <option value={1}>Ordenar por qtd produtos consumidos</option>
+                        <option value={2}>Ordenar por qtd serviços consumidos</option>
+                        <option value={3}>Ordenar por valor gasto</option>
+                    </select>
 
-                ) : (
+                    <table className="tabelaClientes">
+                        <thead>
+                            <tr className="headerTabelaClientes">
+                                <th>Nome</th>
+                                <th>Nome Social</th>
+                                <th>CPF</th>
+                                <th>Excluir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {gerarListaCliente()}
+                        </tbody>
+                    </table>
+                </div>
 
-                    <>
-                        <button className="botaVoltarListagemCliente" onClick={() => { this.setState({ cliente: undefined }) }}>
-                            Voltar
-                        </button>
-                        <AlterarCliente cliente={this.state.cliente} />
-                    </>
+            ) : (
 
-                )}
-            </div>
-        )
-    }
+                <>
+                    <button className="botaVoltarListagemCliente" onClick={() => { setCliente(undefined) }}>
+                        Voltar
+                    </button>
+                    <AlterarCliente cliente={cliente} />
+                </>
+
+            )}
+        </div>
+    )
+
 }
