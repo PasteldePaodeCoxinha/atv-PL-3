f1b51513d5c9848c9f5126e9b1ba2239b2de0225
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-11 17:42:52 -0300

    Atualizei a página de alterar cliente para função

--- src/componentes/cliente/alterar/alterarCliente.tsx
+++ src/componentes/cliente/alterar/alterarCliente.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Cliente from "../../../modelo/cliente";
 import "./alterarCliente.css"
 import Telefone from "../../../modelo/telefone";
@@ -7,166 +7,138 @@ type props = {
     cliente: Cliente
 }
 
-type state = {
-    cliente: Cliente
-    menuTel: Boolean
-    novoDdd: string
-    novoTel: string
-}
+export default function AlterarCliente(props: props) {
+    const cliente = props.cliente
+    const [menuTel, setMenuTel] = useState<Boolean>(false)
+    const [novoDdd, setNovoDdd] = useState<string>("")
+    const [novoTel, setNovoTel] = useState<string>("")
+    const [numeroEscolhido, setNumeroEscolhido] = useState<string>("")
+    const [nome, setNome] = useState<string>(cliente.nome)
+    const [nomeSocial, setNomeSocial] = useState<string>(cliente.nomeSocial)
+    const [email, setEmail] = useState<string>(cliente.getEmail)
 
-export default class AlterarCliente extends Component<props, state> {
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state = {
-            cliente: props.cliente,
-            menuTel: false,
-            novoDdd: "",
-            novoTel: ""
-        }
-        this.menuAdicionarTelefone = this.menuAdicionarTelefone.bind(this)
-        this.adicionarTelefone = this.adicionarTelefone.bind(this)
-        this.deletarTelefone = this.deletarTelefone.bind(this)
-
-        this.mudarValorNome = this.mudarValorNome.bind(this)
-        this.mudarValorNomeSocial = this.mudarValorNomeSocial.bind(this)
-
-        this.mudarValorEmail = this.mudarValorEmail.bind(this)
-
-        this.formatarData = this.formatarData.bind(this)
+    const adicionarTelefone = () => {
+        cliente.getTelefones.push(new Telefone(novoDdd, novoTel))
+        setMenuTel(false)
     }
 
-    adicionarTelefone() {
-        this.state.cliente.getTelefones.push(new Telefone(this.state.novoDdd, this.state.novoTel))
-        this.setState({
-            menuTel: false
-        })
-    }
+    const deletarTelefone = useCallback(() => {
+        cliente.setTelefones = cliente.getTelefones.filter(t => t.getNumero !== numeroEscolhido)
+        setNumeroEscolhido("")
+    }, [cliente, numeroEscolhido])
 
-    deletarTelefone(numero: string) {
-        this.state.cliente.getTelefones.splice((this.state.cliente.getTelefones.findIndex(t => t.getNumero === numero)), 1)
-        this.setState({
-            cliente: this.state.cliente
-        })
-    }
-
-    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
-        const cliente = this.state.cliente
+    const mudarValorNome = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNome(e.target.value)
         cliente.nome = e.target.value
-        this.setState({
-            cliente: cliente
-        })
     }
 
-    mudarValorNomeSocial(e: React.ChangeEvent<HTMLInputElement>) {
-        const cliente = this.state.cliente
+    const mudarValorNomeSocial = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNomeSocial(e.target.value)
         cliente.nomeSocial = e.target.value
-        this.setState({
-            cliente: cliente
-        })
     }
 
-    mudarValorEmail(e: React.ChangeEvent<HTMLInputElement>) {
-        const cliente = this.state.cliente
+    const mudarValorEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
         cliente.setEmail = e.target.value
-        this.setState({
-            cliente: cliente
-        })
     }
 
-    menuAdicionarTelefone() {
+    const menuAdicionarTelefone = () => {
         return (
             <div className="menuAddTel">
-                <input type="text" placeholder="DDD" className="inputAddNovoTelDDD" onChange={e => this.setState({ novoDdd: e.target.value })} />
-                <input type="text" placeholder="Telefone" className="inputAddNovoTelNum" onChange={e => this.setState({ novoTel: e.target.value })} />
-                <button className="botaoConfirmarTel" onClick={this.adicionarTelefone}>Confirmar</button>
+                <input type="text" placeholder="DDD" className="inputAddNovoTelDDD" onChange={e => setNovoDdd(e.target.value)} />
+                <input type="text" placeholder="Telefone" className="inputAddNovoTelNum" onChange={e => setNovoTel(e.target.value)} />
+                <button className="botaoConfirmarTel" onClick={adicionarTelefone}>Confirmar</button>
             </div>
         )
     }
 
-    formatarData(data: Date): string {
+    const formatarData = (data: Date): string => {
         const dataPartes = (data.toISOString().split("T")[0]).split("-")
         const dataCerta = dataPartes[2] + "/" + dataPartes[1] + "/" + dataPartes[0]
         return dataCerta
     }
 
-    render() {
-        return (
-            <div className="containerInformacoesCliente">
-                <div className="campoClienteEditavel">
-                    <label>Nome:</label>
-                    <input type="text" value={this.state.cliente.nome} onChange={this.mudarValorNome} />
-                </div>
+    useEffect(() => {
+        deletarTelefone()
+    }, [deletarTelefone])
 
-                <div className="campoClienteEditavel">
-                    <label>Nome Social:</label>
-                    <input type="text" value={this.state.cliente.nomeSocial} onChange={this.mudarValorNomeSocial} />
-                </div>
+    return (
+        <div className="containerInformacoesCliente">
+            <div className="campoClienteEditavel">
+                <label>Nome:</label>
+                <input type="text" value={nome} onChange={mudarValorNome} />
+            </div>
 
-                <div className="campoClienteEditavel">
-                    <label>Email:</label>
-                    <input type="email" value={this.state.cliente.getEmail} onChange={this.mudarValorEmail} />
-                </div>
+            <div className="campoClienteEditavel">
+                <label>Nome Social:</label>
+                <input type="text" value={nomeSocial} onChange={mudarValorNomeSocial} />
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>CPF:</label>
-                    <p>{this.state.cliente.getCpf.getValor} | {this.formatarData(this.state.cliente.getCpf.getDataEmissao)}</p>
-                </div>
+            <div className="campoClienteEditavel">
+                <label>Email:</label>
+                <input type="email" value={email} onChange={mudarValorEmail} />
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>RG:</label>
-                    <p>{this.state.cliente.getRgs[0].getValor} | {this.formatarData(this.state.cliente.getRgs[0].getDataEmissao)}</p>
-                </div>
+            <div className="campoClienteFixo">
+                <label>CPF:</label>
+                <p>{cliente.getCpf.getValor} | {formatarData(cliente.getCpf.getDataEmissao)}</p>
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>Qtd Produtos Consumidos:</label>
-                    <p>{this.state.cliente.getProdutosConsumidos.length}</p>
-                </div>
+            <div className="campoClienteFixo">
+                <label>RG:</label>
+                <p>{cliente.getRgs[0].getValor} | {formatarData(cliente.getRgs[0].getDataEmissao)}</p>
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>Qtd Serviços consumidos:</label>
-                    <p>{this.state.cliente.getServicosConsumidos.length}</p>
-                </div>
+            <div className="campoClienteFixo">
+                <label>Qtd Produtos Consumidos:</label>
+                <p>{cliente.getProdutosConsumidos.length}</p>
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>Total gasto:</label>
-                    <p>R$ {((this.state.cliente.getValorGasto * 100) * 0.01).toFixed(2).replace(".", ",")}</p>
-                </div>
+            <div className="campoClienteFixo">
+                <label>Qtd Serviços consumidos:</label>
+                <p>{cliente.getServicosConsumidos.length}</p>
+            </div>
 
-                <div className="campoClienteFixo">
-                    <label>Telefone 1:</label>
-                    <p>+{this.state.cliente.getTelefones[0].getDdd} {this.state.cliente.getTelefones[0].getNumero}</p>
-                    {this.state.cliente.getTelefones[1] ?
-                        (
-                            <button onClick={() => this.deletarTelefone(this.state.cliente.getTelefones[0].getNumero)}
-                                className="botaoDeletarTelCliente">
-                                Deletar Telefone
-                            </button>
-                        )
-                        :
-                        (<></>)
-                    }
-                </div>
+            <div className="campoClienteFixo">
+                <label>Total gasto:</label>
+                <p>R$ {((cliente.getValorGasto * 100) * 0.01).toFixed(2).replace(".", ",")}</p>
+            </div>
 
-                {this.state.cliente.getTelefones[1] ?
+            <div className="campoClienteFixo">
+                <label>Telefone 1:</label>
+                <p>+{cliente.getTelefones[0].getDdd} {cliente.getTelefones[0].getNumero}</p>
+                {cliente.getTelefones[1] ?
                     (
-                        <div className="campoClienteFixo">
-                            <label>Telefone 2:</label>
-                            <p>+{this.state.cliente.getTelefones[1].getDdd} {this.state.cliente.getTelefones[1].getNumero}</p>
-                            <button onClick={() => this.deletarTelefone(this.state.cliente.getTelefones[0].getNumero)}
-                                className="botaoDeletarTelCliente">
-                                Deletar Telefone
-                            </button>
-                        </div>
-                    )
-                    :
-                    this.state.menuTel ? (
-                        this.menuAdicionarTelefone()
-                    ) : (
-                        <button onClick={() => this.setState({ menuTel: true })}
-                            className="botaoAddTelCliente">Adicionar Telefone
+                        <button onClick={() => setNumeroEscolhido(cliente.getTelefones[0].getNumero)}
+                            className="botaoDeletarTelCliente">
+                            Deletar Telefone
                         </button>
                     )
+                    :
+                    (<></>)
                 }
-            </div>)
-    }
+            </div>
+
+            {cliente.getTelefones[1] ?
+                (
+                    <div className="campoClienteFixo">
+                        <label>Telefone 2:</label>
+                        <p>+{cliente.getTelefones[1].getDdd} {cliente.getTelefones[1].getNumero}</p>
+                        <button onClick={() => setNumeroEscolhido(cliente.getTelefones[0].getNumero)}
+                            className="botaoDeletarTelCliente">
+                            Deletar Telefone
+                        </button>
+                    </div>
+                )
+                :
+                menuTel ? (
+                    menuAdicionarTelefone()
+                ) : (
+                    <button onClick={() => setMenuTel(true)}
+                        className="botaoAddTelCliente">Adicionar Telefone
+                    </button>
+                )
+            }
+        </div>)
+
 }
