46d2898051c10242ab0f9f84132d0caceb7271db
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-11 22:46:34 -0300

    Atualizei a página de alterar pet para função

--- src/componentes/pet/alterar/alterarPet.tsx
+++ src/componentes/pet/alterar/alterarPet.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./alterarPet.css"
 import Pet from "../../../modelo/pet";
 
@@ -6,66 +6,49 @@ type props = {
     pet: Pet
 }
 
-type state = {
-    pet: Pet
-}
 
-export default class AlterarPet extends Component<props, state> {
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state = {
-            pet: props.pet
-        }
 
-        this.mudarValorNome = this.mudarValorNome.bind(this)
-        this.mudarValorTamanho = this.mudarValorTamanho.bind(this)
+export default function AlterarPet(props: props) {
+    const [nome, setNome] = useState<string>(props.pet.getNome)
+    const [tamanho, setTamanho] = useState<string>(props.pet.getTamanho)
+
+    const mudarValorNome = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.pet.setNome = e.target.value
+        setNome(e.target.value)
     }
 
-    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
-        const pet = this.state.pet
-        pet.setNome = e.target.value
-        this.setState({
-            pet: pet
-        })
+    const mudarValorTamanho = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.pet.setTamanho = e.target.value
+        setTamanho(e.target.value)
     }
 
-    mudarValorTamanho(e: React.ChangeEvent<HTMLInputElement>) {
-        const pet = this.state.pet
-        pet.setTamanho = e.target.value
-        this.setState({
-            pet: pet
-        })
-    }
-
-    render() {
-        return (
-            <div className="containerInformacoesPet">
-                <div className="campoPetEditavel">
-                    <label>Nome:</label>
-                    <input type="text" value={this.state.pet.getNome} onChange={this.mudarValorNome}/>
-                </div>
-
-                <div className="campoPetFixo">
-                    <label>Genêro:</label>
-                    <p>{this.state.pet.getGenero}</p>
-                </div>
-
-                <div className="campoPetEditavel">
-                    <label>Tamanho:</label>
-                    <input type="email" value={this.state.pet.getTamanho} onChange={this.mudarValorTamanho}/>
-                </div>
-
-                <div className="campoPetFixo">
-                    <label>Tipo:</label>
-                    <p>{this.state.pet.getTipo}</p>
-                </div>
-
-                <div className="campoPetFixo">
-                    <label>Raça:</label>
-                    <p>{this.state.pet.getRaca}</p>
-                </div>
-
+    return (
+        <div className="containerInformacoesPet">
+            <div className="campoPetEditavel">
+                <label>Nome:</label>
+                <input type="text" value={nome} onChange={mudarValorNome} />
             </div>
-        )
-    }
+
+            <div className="campoPetFixo">
+                <label>Genêro:</label>
+                <p>{props.pet.getGenero}</p>
+            </div>
+
+            <div className="campoPetEditavel">
+                <label>Tamanho:</label>
+                <input type="email" value={tamanho} onChange={mudarValorTamanho} />
+            </div>
+
+            <div className="campoPetFixo">
+                <label>Tipo:</label>
+                <p>{props.pet.getTipo}</p>
+            </div>
+
+            <div className="campoPetFixo">
+                <label>Raça:</label>
+                <p>{props.pet.getRaca}</p>
+            </div>
+
+        </div>
+    )
 }
