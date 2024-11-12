b886e9a47224cc776a1cc921fb42d9ea9b083949
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-12 11:50:02 -0300

    Atualizei o alterar servicos para função

--- src/componentes/servico/alterar/alterarServico.tsx
+++ src/componentes/servico/alterar/alterarServico.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./alterarServico.css"
 import Servico from "../../../modelo/servico";
 
@@ -8,76 +8,59 @@ type props = {
     listaRacas: Array<Array<string>>
 }
 
-type state = {
-    servico: Servico
-}
+export default function AlterarServico(props: props) {
+    const [nome, setNome] = useState<string>(props.servico.nome)
+    const [preco, setPreco] = useState<number>(props.servico.preco)
 
-export default class AlterarServico extends Component<props, state> {
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state = {
-            servico: props.servico
-        }
-        this.mudarValorNome = this.mudarValorNome.bind(this)
-        this.mudarValorPreco = this.mudarValorPreco.bind(this)
+    const mudarValorNome = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.servico.nome = e.target.value
+        setNome(e.target.value)
     }
 
-    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
-        const servico = this.state.servico
-        servico.nome = e.target.value
-        this.setState({
-            servico: servico
-        })
+    const mudarValorPreco = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.servico.preco = Number(e.target.value).valueOf()
+        setPreco(Number(e.target.value).valueOf())
     }
 
-    mudarValorPreco(e: React.ChangeEvent<HTMLInputElement>) {
-        const servico = this.state.servico
-        servico.preco = Number(e.target.value).valueOf()
-        this.setState({
-            servico: servico
-        })
-    }
-
-    render() {
-        return (
-            <div className="containerInformacoesServico">
-                <div className="campoServicoEditavel">
-                    <label>Nome:</label>
-                    <input type="text" value={this.state.servico.nome} onChange={this.mudarValorNome} />
-                </div>
-
-                <div className="campoServicoEditavel">
-                    <label>Tamanho:</label>
-                    <input type="number" value={this.state.servico.preco} onChange={this.mudarValorPreco} />
-                </div>
-
-                <div className="campoServicoFixo">
-                    <label>Quantidade vendido:</label>
-                    <p>{this.state.servico.getCompraram}</p>
-                </div>
-
-                {this.props.listaTipos.map(t => {
-                    return (<>
-                        <div className="campoServicoFixo">
-                            <label>{t} compraram:</label>
-                            <p>{(this.state.servico.getRacasCompraram.filter(r => r[0] === t)).length}</p>
-                        </div>
-                        {this.props.listaRacas.filter(r => r[0] === t).map(ra => {
-                            return (
-                                <div className="subCampoServicoFixo">
-                                    <label>{ra[1]} compraram:</label>
-                                    <p>{(this.state.servico.getRacasCompraram.filter(r => r[1] === ra[1])).length}</p>
-                                </div>
-                            )
-                        })
-
-                        }
-                    </>)
-                })
-
-                }
-
+    return (
+        <div className="containerInformacoesServico">
+            <div className="campoServicoEditavel">
+                <label>Nome:</label>
+                <input type="text" value={nome} onChange={mudarValorNome} />
             </div>
-        )
-    }
+
+            <div className="campoServicoEditavel">
+                <label>Preço:</label>
+                <input type="number" value={preco} onChange={mudarValorPreco} />
+            </div>
+
+            <div className="campoServicoFixo">
+                <label>Quantidade vendido:</label>
+                <p>{props.servico.getCompraram}</p>
+            </div>
+
+            {props.listaTipos.map(t => {
+                return (<>
+                    <div className="campoServicoFixo">
+                        <label>{t} compraram:</label>
+                        <p>{(props.servico.getRacasCompraram.filter(r => r[0] === t)).length}</p>
+                    </div>
+                    {props.listaRacas.filter(r => r[0] === t).map(ra => {
+                        return (
+                            <div className="subCampoServicoFixo">
+                                <label>{ra[1]} compraram:</label>
+                                <p>{(props.servico.getRacasCompraram.filter(r => r[1] === ra[1])).length}</p>
+                            </div>
+                        )
+                    })
+
+                    }
+                </>)
+            })
+
+            }
+
+        </div>
+    )
+
 }
