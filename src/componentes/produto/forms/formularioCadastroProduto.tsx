dfc3eaa278f19f2aaa222f9a356bc9c1436d807a
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-12 10:20:32 -0300

    Atualizei a página de formsCadastroProduto para função

--- src/componentes/produto/forms/formularioCadastroProduto.tsx
+++ src/componentes/produto/forms/formularioCadastroProduto.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from "react";
+import { useState } from "react";
 import "./formularioCadastroProduto.css"
 import Produto from "../../../modelo/produto";
 
@@ -7,87 +7,66 @@ type props = {
     produtos: Produto[]
 }
 
-type state = {
-    nome: string,
-    preco: number,
-}
+export default function FormularioCadastroProduto(props: props) {
+    const [nome, setNome] = useState<string>("")
+    const [preco, setPreco] = useState<number>(0)
 
-export default class FormularioCadastroProduto extends Component<props, state> {
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state = {
-            nome: "",
-            preco: 0
-        }
-        this.mudarValorNome = this.mudarValorNome.bind(this)
-        this.mudarValorPreco = this.mudarValorPreco.bind(this)
-
-        this.adicionarProduto = this.adicionarProduto.bind(this)
+    const mudarValorNome = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNome(e.target.value)
     }
 
-    mudarValorNome(e: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            nome: e.target.value
-        })
-    }
-
-    mudarValorPreco(e: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            preco: Number(e.target.value).valueOf()
-        })
+    const mudarValorPreco = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPreco(Number(e.target.value).valueOf())
     }
 
 
-    adicionarProduto(e: React.ChangeEvent<HTMLFormElement>) {
+    const adicionarProduto = (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault()
-        
-        if (this.props.produtos.find(p => p.nome === this.state.nome)) {
+
+        if (props.produtos.find(p => p.nome === nome)) {
             alert("Esse produto já está registrado")
             return
         }
 
-        const produto = new Produto(this.state.nome, this.state.preco)
+        const produto = new Produto(nome, preco)
 
-        this.props.produtos.push(produto)
+        props.produtos.push(produto)
 
-        this.setState({
-            nome: "",
-            preco: 0
-        })
+        setNome("")
+        setPreco(0)
     }
 
-    render() {
-        return (
-            <div className="containerFormularioProduto">
-                <form className="formularioProduto" onSubmit={this.adicionarProduto}>
+    return (
+        <div className="containerFormularioProduto">
+            <form className="formularioProduto" onSubmit={adicionarProduto}>
 
-                    <div className="linhaFormularioCadastroProduto">
+                <div className="linhaFormularioCadastroProduto">
 
-                        <input type="text"
-                            className="inputProdutoForms"
-                            placeholder="Nome"
-                            value={this.state.nome}
-                            onChange={this.mudarValorNome}
-                            required
-                            />
+                    <input type="text"
+                        className="inputProdutoForms"
+                        placeholder="Nome"
+                        value={nome}
+                        onChange={mudarValorNome}
+                        required
+                    />
 
-                        <input type="number"
-                            className="inputProdutoForms"
-                            placeholder="Porduto"
-                            value={this.state.preco}
-                            onChange={this.mudarValorPreco}
-                            required
-                            
-                            />
+                    <input type="number"
+                        className="inputProdutoForms"
+                        placeholder="Porduto"
+                        value={preco}
+                        onChange={mudarValorPreco}
+                        required
 
-                    </div>
+                    />
 
-                    <div className="containerBotaoCadastrarProduto">
-                        <button className="botaoCadastrarProduto">CADASTRAR</button>
-                    </div>
-                </form>
+                </div>
 
-            </div>
-        )
-    }
+                <div className="containerBotaoCadastrarProduto">
+                    <button className="botaoCadastrarProduto">CADASTRAR</button>
+                </div>
+            </form>
+
+        </div>
+    )
+
 }
